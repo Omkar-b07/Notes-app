ec7b91b9e5a60067fc13790dce177fa3000d04f5
Omkar Bondge <omkarbondge07@gmail.com>
2026-07-05 22:50:38 +0530

    Initialize React frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./pages/Login";
+function App() {
+  return (
+    <Login 
+    name ="Omkar"
+    age = '21'
+    city = 'pune'
+    />
+
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+function Login(props) {
+    return (
+        <>
+            <h1>Welcome {props.name}</h1>
+            <h2>Age:{props.age}</h2>
+            <h3>City:{props.city}</h3>
+        </>
+
+    );
+}
+
+export default Login;
