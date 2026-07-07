8d344c86d53ffa538ef96346885cdaa8d6271bc0
Omkar Bondge <omkarbondge07@gmail.com>
2026-07-07 19:17:10 +0530

    signup page post route created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
 function App() {
   return (
+    <>
     <Login 
     name ="Omkar"
     age = '21'
     city = 'pune'
     />
+    <Signup/>
+    </>
+    
 
   );
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,19 +1,43 @@
 import { useState } from "react";
+import axios from "axios";
 function Signup() {
     const [name, setName] = useState("");
-    const [email,setEmail]=useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSignup() {
+        axios.post(
+            "http://localhost:5000/api/auth/signUp",
+            { name, email, password }
+        );
+    }
 
     return (
         <>
             <h1>Signup page</h1>
             <input
                 type="text"
+                value={name}
                 placeholder="enter name"
-                onChange={(e)=>setName(e.target.value)}
+                onChange={(e) => setName(e.target.value)}
             ></input>
-
+            <input
+                type="email"
+                value={email}
+                placeholder="enter email"
+                onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <input
+                type="password"
+                value={password}
+                placeholder="enter password"
+                onChange={(e) => setPassword(e.target.value)}
+            >
+            </input>
+            <button onClick={handleSignup}>
+                Signup
+            </button>
         </>
-
     );
 }
 export default Signup;
